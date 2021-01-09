107d0bbdcbfc2d7dc274b92f751b22fc09bd8289
vetneka <vetneka@yandex.ru>
2021-01-09 22:37:26 +0500

    feat: add user greeting

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,6 @@
 #!/usr/bin/env node
+import { getUserName, greetingUser } from './../src/cli.js';
+
 console.log('Welcome to the Brain Games!');
+const userName = getUserName();
+greetingUser(userName);
