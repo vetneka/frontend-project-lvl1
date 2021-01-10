981da936ea23f68c09b23ea12c2762a166658dba
vetneka <vetneka@yandex.ru>
2021-01-10 12:56:53 +0500

    fix: fix eslint errors

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { getUserName, greetingUser } from './../src/cli.js';
+import { getUserName, greetingUser } from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
 const userName = getUserName();
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
-const USER_NAME_QUESTION_TEXT = `May I have you name? `;
-const DEFAULT_USER_NAME = `Noname`;
+const USER_NAME_QUESTION_TEXT = 'May I have you name? ';
+const DEFAULT_USER_NAME = 'Noname';
 
 const getUserName = () => readlineSync.question(USER_NAME_QUESTION_TEXT, {
   defaultInput: DEFAULT_USER_NAME,
