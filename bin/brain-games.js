4472c60f8a9359c265f7457dac904d467bb8bed1
vetneka <vetneka@yandex.ru>
2021-01-11 17:16:03 +0500

    refactor: refactor code with new functions

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
-import { getUserName, greetingUser } from '../src/cli.js';
+import {
+  COMMON_GREETING,
+  USER_GREETING,
+  USER_NAME_QUESTION_TEXT,
+  DEFAULT_USER_NAME,
+} from '../src/consts.js';
+import { print, askUser } from '../src/cli.js';
 
-console.log('Welcome to the Brain Games!');
-const userName = getUserName();
-greetingUser(userName);
+print(COMMON_GREETING);
+const userName = askUser(USER_NAME_QUESTION_TEXT, DEFAULT_USER_NAME);
+print(`${USER_GREETING}, ${userName}`);
