4f3800ce63b25f9acb99e9eb63fa4ed84611350f
vetneka <vetneka@yandex.ru>
2021-01-12 14:14:03 +0500

    refactor: separate common constants from games constants

--- src/consts.js
+++ src/consts.js
@@ -6,19 +6,19 @@ const USER_GREETING = 'Hello';
 const DEFAULT_USER_NAME = 'Noname';
 const USER_NAME_QUESTION_TEXT = 'May I have you name?';
 
+const GAME_QUESTION_TEXT = 'Question';
+const GAME_ANSWER_TEXT = 'You answer';
+
+const GAME_TRUE_MESSAGE = 'Correct!';
+const GAME_FALSE_MESSAGE = 'is wrong answer ;(. Correct answer was';
+
+const GAME_TRUE_END_MESSAGE = 'Congratulations';
+const GAME_FALSE_END_MESSAGE = 'Let\'s try again';
+
 // EVEN_GAME
 const EVEN_GAME_RULES = 'Answer "yes" if the number is even, otherwise answer "no".';
-const EVEN_GAME_QUESTION = 'Question';
-const EVEN_GAME_QUESTION_USER = 'You answer';
-
 const EVEN_GAME_TRUE_ANSWER = 'yes';
 const EVEN_GAME_FALSE_ANSWER = 'no';
-
-const EVEN_GAME_TRUE_MESSAGE = 'Correct!';
-const EVEN_GAME_FALSE_MESSAGE = 'is wrong answer ;(. Correct answer was';
-
-const EVEN_GAME_TRUE_END_MESSAGE = 'Congratulations';
-const EVEN_GAME_FALSE_END_MESSAGE = 'Let\'s try again';
 
 // CALC_GAME
 const CALC_GAME_RULES = 'What is the result of the expression?';
@@ -29,15 +29,18 @@ export {
   DEFAULT_USER_NAME,
   USER_NAME_QUESTION_TEXT,
 
+  GAME_QUESTION_TEXT,
+  GAME_ANSWER_TEXT,
+
+  GAME_TRUE_MESSAGE,
+  GAME_FALSE_MESSAGE,
+
+  GAME_TRUE_END_MESSAGE,
+  GAME_FALSE_END_MESSAGE,
+
   EVEN_GAME_RULES,
-  EVEN_GAME_QUESTION,
-  EVEN_GAME_QUESTION_USER,
   EVEN_GAME_TRUE_ANSWER,
   EVEN_GAME_FALSE_ANSWER,
-  EVEN_GAME_TRUE_MESSAGE,
-  EVEN_GAME_FALSE_MESSAGE,
-  EVEN_GAME_TRUE_END_MESSAGE,
-  EVEN_GAME_FALSE_END_MESSAGE,
 
   CALC_GAME_RULES,
 };
