a57a384eb02ac659c83bb6fe98574ede7bf6d44b
vetneka <vetneka@yandex.ru>
2021-01-18 10:08:20 +0500

    refactor: make game brain-even testable

--- src/games/brain-even-game.js
+++ src/games/brain-even-game.js
@@ -1,13 +1,20 @@
 import { EVEN_GAME_TRUE_ANSWER, EVEN_GAME_FALSE_ANSWER } from '../consts.js';
-import { getRandomIntNumber, isEven } from '../utils.js';
+import { getRandomIntNumber } from '../utils.js';
 
-const brainEvenGame = () => {
-  const [GAME_RANGE_MIN, GAME_RANGE_MAX] = [0, 100];
+const [GAME_RANGE_MIN, GAME_RANGE_MAX] = [0, 100];
 
-  const question = getRandomIntNumber(GAME_RANGE_MIN, GAME_RANGE_MAX);
+const isEven = (number) => number % 2 === 0;
+
+const generateGameData = () => getRandomIntNumber(GAME_RANGE_MIN, GAME_RANGE_MAX);
+
+const brainEvenGame = (data) => {
+  const question = data;
   const expectedAnswer = isEven(question) ? EVEN_GAME_TRUE_ANSWER : EVEN_GAME_FALSE_ANSWER;
 
   return [question, expectedAnswer];
 };
 
-export default brainEvenGame;
+export {
+  generateGameData,
+  brainEvenGame as default,
+};
