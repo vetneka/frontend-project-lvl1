a8c182131088d5d7a1f2bb70906cdf4ccd3b38ec
vetneka <vetneka@yandex.ru>
2021-01-18 10:13:54 +0500

    refactor: replace isEven() from utils to even-game file

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,3 @@
-const isEven = (number) => number % 2 === 0;
-
 const getRandomIntNumber = (min = 0, max = 10) => {
   const randomIntNumber = min + Math.random() * (max + 1 - min);
   return Math.floor(randomIntNumber);
@@ -11,7 +9,6 @@ const getRandomArrayValue = (array) => {
 };
 
 export {
-  isEven,
   getRandomIntNumber,
   getRandomArrayValue,
 };
