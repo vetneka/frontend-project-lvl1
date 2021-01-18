8b19e2413fc661207c90b13901448645a2affa76
vetneka <vetneka@yandex.ru>
2021-01-18 10:09:05 +0500

    refactor: make game brain-gcd testable

--- src/games/brain-gcd-game.js
+++ src/games/brain-gcd-game.js
@@ -1,5 +1,7 @@
 import { getRandomIntNumber } from '../utils.js';
 
+const [GAME_RANGE_MIN, GAME_RANGE_MAX] = [0, 10];
+
 const getGCD = (x, y) => {
   let tempX = x;
   let tempY = y;
@@ -17,11 +19,15 @@ const getGCD = (x, y) => {
   return tempX;
 };
 
-const brainGCDGame = () => {
-  const [GAME_RANGE_MIN, GAME_RANGE_MAX] = [0, 10];
-
+const generateGameData = () => {
   const firstNumber = getRandomIntNumber(GAME_RANGE_MIN, GAME_RANGE_MAX);
   const secondNumber = getRandomIntNumber(GAME_RANGE_MIN, GAME_RANGE_MAX);
+
+  return [firstNumber, secondNumber];
+};
+
+const brainGCDGame = (data) => {
+  const [firstNumber, secondNumber] = data;
 
   const question = `${firstNumber} ${secondNumber}`;
   const expectedAnswer = getGCD(firstNumber, secondNumber);
@@ -29,4 +35,7 @@ const brainGCDGame = () => {
   return [question, expectedAnswer];
 };
 
-export default brainGCDGame;
+export {
+  generateGameData,
+  brainGCDGame as default,
+};
