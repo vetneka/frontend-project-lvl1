df5823887a43ff755d331eb5c8058fbfcee03e63
vetneka <vetneka@yandex.ru>
2021-01-18 10:10:02 +0500

    refactor: make game brain-progression testable

--- src/games/brain-progression-game.js
+++ src/games/brain-progression-game.js
@@ -1,5 +1,9 @@
 import { getRandomIntNumber } from '../utils.js';
 
+const [PROGRESSION_LENGTH_MIN, PROGRESSION_LENGTH_MAX] = [5, 10];
+const [PROGRESSION_RANGE_MIN, PROGRESSION_RANGE_MAX] = [0, 100];
+const [COMMON_DIFFERENCE_MIN, COMMON_DIFFERENCE_MAX] = [1, 10];
+
 const getProgressionTerm = (a1, n, d) => a1 + (n - 1) * d;
 
 const createProgression = (length, firstTerm, commonDifference) => {
@@ -13,19 +17,31 @@ const createProgression = (length, firstTerm, commonDifference) => {
   return progression;
 };
 
-const brainProgressionGame = () => {
-  const [PROGRESSION_LENGTH_MIN, PROGRESSION_LENGTH_MAX] = [5, 10];
-  const [PROGRESSION_RANGE_MIN, PROGRESSION_RANGE_MAX] = [0, 100];
-  const [COMMON_DIFFERENCE_MIN, COMMON_DIFFERENCE_MAX] = [1, 10];
-  const placeholder = '..';
-
+const generateGameData = () => {
   const progressionLength = getRandomIntNumber(PROGRESSION_LENGTH_MIN, PROGRESSION_LENGTH_MAX);
   const progressionFirstItem = getRandomIntNumber(PROGRESSION_RANGE_MIN, PROGRESSION_RANGE_MAX);
   const commonDifference = getRandomIntNumber(COMMON_DIFFERENCE_MIN, COMMON_DIFFERENCE_MAX);
+  const missingItemIndex = getRandomIntNumber(0, progressionLength - 1);
+
+  return [
+    progressionLength,
+    progressionFirstItem,
+    commonDifference,
+    missingItemIndex,
+  ];
+};
+
+const brainProgressionGame = (data) => {
+  const placeholder = '..';
+  const [
+    progressionLength,
+    progressionFirstItem,
+    commonDifference,
+    missingItemIndex,
+  ] = data;
 
   const progression = createProgression(progressionLength, progressionFirstItem, commonDifference);
 
-  const missingItemIndex = getRandomIntNumber(0, progressionLength - 1);
   const missingItem = progression[missingItemIndex];
   progression[missingItemIndex] = placeholder;
 
@@ -35,4 +51,7 @@ const brainProgressionGame = () => {
   return [question, expectedAnswer];
 };
 
-export default brainProgressionGame;
+export {
+  generateGameData,
+  brainProgressionGame as default,
+};
