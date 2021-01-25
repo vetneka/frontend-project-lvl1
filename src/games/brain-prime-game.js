886db356a66c1acabd365457c7b348cc76f5b4c6
vetneka <vetneka@yandex.ru>
2021-01-25 19:49:02 +0500

    refactor: change algorithm for finding prime number

--- src/games/brain-prime-game.js
+++ src/games/brain-prime-game.js
@@ -4,14 +4,13 @@ import { getRandomIntNumber } from '../utils.js';
 const [GAME_RANGE_MIN, GAME_RANGE_MAX] = [0, 10];
 
 const isPrime = (number) => {
-  if (number === 2) return true;
-  if (number <= 1 || number % 2 === 0) return false;
-
-  for (let i = 3; i <= Math.sqrt(number); i += 1) {
-    if (number % i === 0) return false;
+  for (let i = 2; i <= Math.sqrt(number); i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
   }
 
-  return true;
+  return number > 1;
 };
 
 const generateGameData = () => getRandomIntNumber(GAME_RANGE_MIN, GAME_RANGE_MAX);
